2a61438288e01ee46b7ffdea41b21fbcf73a74d0
danutzu117 <danutzu117@yahoo.com>
2016-10-29 18:49:22 +0300

    Update apache_check.sh

--- apache_check.sh
+++ apache_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Let's declare RESTART, PGREP and HTTPD command names.
+#Declaram variabilele
 
 RESTART="service httpd restart"
 
@@ -8,19 +8,17 @@ PGREP="/usr/bin/pgrep"
 HTTPD="httpd"
 
 NETSTAT="netstat -ln | grep ':80'"
-#PORT="80"
+PORT="80"
 
 STATUS="curl -I  --stderr /dev/null http://www.start-learning.ro | head -1 | cut -d' ' -f2"
-#COD="200"
+COD="200"
 
 WORD="wget "http://www.start-learning.ro" --timeout 5 -O - 2>/dev/null | grep works"
 
-
-# find httpd pid
-
+# cautam pid-ul httpd-ului si ULR status
 $PGREP ${HTTPD}
-#$NETSTAT ${PORT}
-$SATUS ${COD}
+$NETSTAT ${PORT}
+$STATUS ${COD}
 
 if [ $? -ne 0 ]
 then
@@ -30,23 +28,22 @@ exit
 
 else
 
-#Check if the process retruns not value[httpd not running].
-
-#if [[ $? -ne 0 || $NETSTAT -ne 80 || $STATUS -ne 200 || $WORD -ne works ]]
-#if [[ $NETSTAT -ne 80 || $STATUS -ne 200 || $WORD -ne works ]]
-
-if [[ $? -ne 0 || $NETSTAT -ne 1 || $STATUS -ne 0 || $WORD -ne works ]]
+if [[ $? -ne 0 || $NETSTAT -ne 1 || $STATUS -ne 1 || $WORD -ne works ]]
 
 then
 
- # restart apache
  $RESTART
 
+#exit
 #for n in {1..5};
 #do
-# $RESTART;
+ #$RESTART;
 #done
 #echo "Apache don't start " | mail -s "Server Alert: Serverul Apache nu mai ruleaza" dincadaniel88@gmail.com
 
 fi
 fi
+
+# 02 08,17 * * * /scripts/apache_check.sh  -> am setat un cronjob (crontab -e) pe server care sa faca #check-ul la 2 minute pe fiecare zi intre orele 8:00AM -> 5:00PM
+
+# https://github.com/danutzu117/sx1/blob/master/apache_check.sh  GIT file
